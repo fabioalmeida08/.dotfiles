a96201c80b78cd494c81aaa074865c757d3df557
fabio <fabioalmeida0896@gmail.com>
2023-02-25 19:55:38 -0300

    add polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Termine instâncias de barras em execução
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar"
+
+# Terminate already running bar instances
 killall -q polybar
 
-# Espere até que os processos em execução sejam terminados
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# execute a Polybar, usando a configuração padrão ~/.config/polybar/config
-polybar example &
-
-echo "Polybar lançada..."
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
