f49cb6f605306affafc60bd03de37733e8f260c8
fabio <fabioalmeida0896@gmail.com>
2024-01-27 12:21:14 -0300

    updated new neovim dotfiles

---
+++ nvim/lua/custom/mappings.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+-- In order to disable a default keymap, use
+M.disabled = {
+  n = {
+      ["<leader>q"] = "",
+  }
+}
+-- Your custom mappings
+M.personal = {
+  n = {
+    ["<C-q>"] = {"<cmd> qall <CR>", "Quit nvim"},
+    ["<A-j>"] = {"<cmd> m +1 <CR>", "Move cursor one line down"},
+    ["<A-k>"] = {"<cmd> m -2 <CR>", "Move crusor one line up"},
+    ["<leader>\\"] = {"<cmd> vsplit <CR>", "Split terminal horizontal"},
+    ["<leader>q"] = {"<cmd> close <CR>", "close"},
+    ["<leader>ft"] = {"<cmd> TodoTelescope <CR>", "Search for todo"},
+    ["<leader>tt"] = {"<cmd> TroubleToggle <CR>", "Trouble Toggle"},
+    ["<leader>gl"] = {"<cmd> LazyGit <CR>", "Lazy git"},
+    ["<leader>rh"] = {"<cmd> vertical resize -10 <CR>", "vertical resize -"},
+    ["<leader>rl"] = {"<cmd> vertical resize +10 <CR>", "vertical resize +"},
+    ["<leader>rj"] = {"<cmd> horizontal resize -10 <CR>", "horizontal resize -"},
+    ["<leader>rk"] = {"<cmd> horizontal resize +10 <CR>", "horizontal resize +"},
+  },
+}
+  
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {"<cmd> DapToggleBreakpoint <CR>"}
+  }
+}
+
+M.dap_python = {
+  plugin = true,
+  n = {
+    ["<leader>dpr"] = {
+      function()
+        require('dap-python').test_method()
+      end
+    }
+  }
+}
+return M
+
---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,195 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "pyright",
+        "black",
+        "debugpy",
+        "eslint-lsp",
+        "prettierd",
+        "tailwindcss-language-server",
+        "typescript-language-server",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "rust-lang/rust.vim",
+    ft= "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },  
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    event = "BufEnter",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    lazy = false,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup()
+    end
+  },
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("trouble").setup()
+    end
+  },
+  {
+  "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          hover = {
+              enabled = false,
+          },
+          signature = {
+              enabled = false,
+          },
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        }
+      })
+    end
+  },
+  { 
+    'echasnovski/mini.nvim', 
+    version = false ,
+    lazy = false ,
+    config = function()
+      require('mini.indentscope').setup() 
+      require('mini.animate').setup()
+    end
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = false,
+    dependencies = {
+       "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("lazy").setup()
+    end
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    ft = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function()
+      local opts = require "plugins.configs.treesitter"
+      opts.ensure_installed = {
+        "lua",
+        "javascript",
+        "typescript",
+        "tsx",
+        "rust"
+      }
+      return opts
+    end,
+  },
+  {
+    "booperlv/nvim-gomove",
+    lazy = false,
+    config = function()
+      require("gomove").setup({
+        -- whether or not to map default key bindings, (true/false)
+        map_defaults = true,
+        -- whether or not to reindent lines moved vertically (true/false)
+        reindent = true,
+        -- whether or not to undojoin same direction moves (true/false)
+        undojoin = true,
+        -- whether to not to move past end column when moving blocks horizontally, (true/false)
+        move_past_end_col = false,
+      })
+    end,
+  }
+}
+return plugins
