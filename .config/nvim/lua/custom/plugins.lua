874f7850c5bb9fade9a502b347d5e39ec0420038
fabio <>
2024-07-04 17:08:57 -0300

    add config for autoformat python

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -202,6 +202,27 @@ local plugins = {
         move_past_end_col = false,
       })
     end,
+  },
+  {
+    'stevearc/conform.nvim',
+    event = {"BufReadPre", "BufNewFile"},
+    opts = {},
+    config = function ()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+          -- Conform will run multiple formatters sequentially
+          python = {"black" },
+          -- Use a sub-list to run only the first available formatter
+          javascript = { { "prettierd", "prettier" } },
+        },
+        format_on_save = {
+          -- These options will be passed to conform.format()
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        },
+      })
+    end
   }
 }
 return plugins
