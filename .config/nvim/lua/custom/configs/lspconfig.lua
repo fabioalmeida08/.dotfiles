a9f6702acdc7c507d5590749a5d08ccf24198eeb
fabio <>
2025-03-28 21:18:35 -0300

    add config for c lang lsp

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -25,6 +25,17 @@ lspconfig.pyright.setup({
 
 local servers = {"ts_ls", "tailwindcss", "eslint"}
 
+-- Configuração do clangd
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+      -- Função chamada quando o LSP se conecta ao buffer
+      print("LSP clangd conectado!")
+  end,
+  cmd = { "clangd" }, -- Comando para iniciar o clangd
+  filetypes = { "c", "cpp", "objc", "objcpp" }, -- Tipos de arquivo suportados
+  root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"), -- Diretório raiz do projeto
+}
+
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
