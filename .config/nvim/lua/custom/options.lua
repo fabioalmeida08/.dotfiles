cab5f40396a13081b871d392d195fd821691c2b7
fabio <>
2025-05-31 14:28:43 -0300

    add config to exclude empty lines on the end of the file before saving

--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -16,13 +16,25 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = { "*.c", "*.h", "*.cpp" },
+--   callback = function()
+--     local last_line_num = vim.fn.line('$')
+--     local last_line = vim.fn.getline(last_line_num)
+--     if last_line == '' then
+--       vim.api.nvim_buf_set_lines(0, last_line_num - 1, last_line_num, false, {})
+--     end
+--   end,
+-- })
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = { "*.c", "*.h", "*.cpp" },
   callback = function()
-    local last_line_num = vim.fn.line('$')
-    local last_line = vim.fn.getline(last_line_num)
-    if last_line == '' then
-      vim.api.nvim_buf_set_lines(0, last_line_num - 1, last_line_num, false, {})
+    local last = vim.fn.line('$')
+    while last > 1 and vim.fn.getline(last):match("^%s*$") do
+      last = last - 1
     end
+    -- Apaga todas as linhas a partir de 'last + 1' até o fim do buffer
+    vim.api.nvim_buf_set_lines(0, last, -1, false, {})
   end,
 })
