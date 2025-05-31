e7a7319efb6a23a066f63154594efde846c97502
fabio <>
2025-05-31 13:10:13 -0300

    add action to remove last line when empty and alias for norminette

--- .config/nvim/lua/custom/options.lua
+++ .config/nvim/lua/custom/options.lua
@@ -15,3 +15,14 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 2
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.c", "*.h", "*.cpp" },
+  callback = function()
+    local last_line_num = vim.fn.line('$')
+    local last_line = vim.fn.getline(last_line_num)
+    if last_line == '' then
+      vim.api.nvim_buf_set_lines(0, last_line_num - 1, last_line_num, false, {})
+    end
+  end,
+})
