e4bdc1b37937da8819e7ac8395a8c4521e8e00e6
fabio <fabioalmeida0896@gmail.com>
2025-12-27 22:42:14 -0300

    add mako script and swaylock script

---
+++ .scripts/lock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+swaylock \
+  --color 000000bf \
+  --font "JetBrainsMono Nerd Font" \
+  --indicator-radius 100 \
+  --indicator-thickness 7 \
+  --ring-color ffffff \
+  --key-hl-color 888888 \
+  --text-color ffffff \
+  --line-color 00000000 \
+  --inside-color 00000088 \
+  --separator-color 00000000
